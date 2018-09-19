6c51aab791941cfe184f85b9bfdb9ea8dbb88931
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2018-09-19 00:07:22 -0400

    Creates search and favourites component

--- shopify-challenge/src/app/app.module.ts
+++ shopify-challenge/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { FavouritesComponent } from './favourites/favourites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    FavouritesComponent
   ],
   imports: [
     BrowserModule
