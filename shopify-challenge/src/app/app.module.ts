b7dc5dab097d07e21cc95808e9dc08b4aa98f977
kkjasoncheung <kkjason.cheung@mail.utoronto.ca>
2018-09-22 17:09:53 -0400

    Adds data binding for field input

--- shopify-challenge/src/app/app.module.ts
+++ shopify-challenge/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
@@ -14,7 +15,8 @@ import { SearchResultsComponent } from './search-results/search-results.componen
     SearchResultsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
